ea80e5404545b6d4a19260091d1fbda794e1d869
jenishashirley <jenisha1603@gmail.com>
2022-08-25 18:58:47 +0530

    first program

---
+++ first.java
@@ -0,0 +1,10 @@
+class first
+{
+    public static void main(string args[])
+    {
+        int a,b,c 
+        a=10
+        b=20
+        c=a+b
+        System.out.println("Sum of "+num1+" and "+num2+" is: "+sum);
+}
